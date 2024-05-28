86c115e3adc746eb14f40e52aacefa9d69d32849
Rastko <rastko.strbac@gmail.com>
2024-05-28 16:41:56 +0200

    Create function to populate display on click
    Create function which will, on the click of a number, add the number to
    the display.

--- script.js
+++ script.js
@@ -40,3 +40,32 @@ function operate(number1, operator, number2){
             return divide(number1,number2);
     }
 }
+
+let displayValue = "";
+
+let displayText = document.getElementById("display-text");
+
+function display(number){
+    displayValue += number;
+    displayText.textContent = displayValue;
+}
+
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+
+one.addEventListener("click", () => display("1"));
+two.addEventListener("click", () => display("2"));
+three.addEventListener("click", () => display("3"));
+four.addEventListener("click", () => display("4"));
+five.addEventListener("click", () => display("5"));
+six.addEventListener("click", () => display("6"));
+seven.addEventListener("click", () => display("7"));
+eight.addEventListener("click", () => display("8"));
+nine.addEventListener("click", () => display("9"));
