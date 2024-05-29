00ec85085cc4c8831d30342b7965ba6da0360652
Rastko <rastko.strbac@gmail.com>
2024-05-29 18:53:58 +0200

    Add keyboard input for numbers and fix bug

--- script.js
+++ script.js
@@ -152,3 +152,42 @@ function deleteCharacter(){
 }
 
 backspace.addEventListener("click", () => deleteCharacter());
+
+function handleKeyPress(event){
+    switch(event.key){
+        case "0":
+            display("0");
+            break;
+        case "1":
+            display("1");
+            break;
+        case "2":
+            display("2");
+            break;
+        case "3":
+            display("3");
+            break;
+        case "4":
+            display("4");
+            break;
+        case "5":
+            display("5");
+            break;
+        case "6":
+            display("6");
+            break;
+        case "7":
+            display("7");
+            break;
+        case "8":
+            display("8");
+            break;
+        case "9":
+            display("9");
+            break;
+        default:
+            break;
+    }
+}
+
+document.addEventListener("keydown", handleKeyPress);
